1b29b926f9474696fe96a603cf84ba70b46fad13
Faraz Aly <farazali12101@gmail.com>
2025-12-17 19:32:28 +0300

    commit

--- Faraz Javed - Web Developer.js
+++ Faraz Javed - Web Developer.js
@@ -5,8 +5,6 @@ const observer = new IntersectionObserver((entries) => {
         // console.log(entry);
         if(entry.isIntersecting){
             entry.target.classList.add('show');
-        } else{
-            entry.target.classList.remove('show');
         }
     });
 });
@@ -25,8 +23,6 @@ const skillObserver = new IntersectionObserver((skillEntries) => {
         // console.log(skillEntry);
         if(skillEntry.isIntersecting){
             skillEntry.target.classList.add('skill-show');
-        } else{
-            skillEntry.target.classList.remove('skill-show');
         }
     });
 });
@@ -37,3 +33,45 @@ SkillElements.forEach((skillEl) => skillObserver.observe(skillEl));
 //////////////////////// skill section animation
 
 
+
+///////////////////////// project section animation
+
+const projectObserver = new IntersectionObserver((skillEntries) => {
+
+    skillEntries.forEach((skillEntry) => {
+        // console.log(skillEntry);
+        if(skillEntry.isIntersecting){
+            skillEntry.target.classList.add('project-show');
+        }
+    });
+});
+
+const projectElements = document.querySelectorAll('.project-hidden');
+projectElements.forEach((projectEl) => projectObserver.observe(projectEl));
+
+
+
+const outlineObserver = new IntersectionObserver((skillEntries) => {
+    const logoText = document.getElementById('logo-text');
+    const contactBtn = document.getElementById('contact-btn');
+
+    skillEntries.forEach((skillEntry) => {
+        // console.log(skillEntry);
+        if(skillEntry.isIntersecting){
+            logoText.classList.add('green');
+            contactBtn.classList.add('green-btn');
+            logoText.classList.remove('logo-text');    
+            contactBtn.classList.remove('contact-btn');  
+        } else{
+            logoText.classList.remove('green');
+            contactBtn.classList.remove('green-btn');
+            logoText.classList.add('logo-text');
+            contactBtn.classList.add('contact-btn');
+        }
+    });
+});
+
+const outlineElements = document.querySelectorAll('.no-green');
+outlineElements.forEach((outlineEl) => outlineObserver.observe(outlineEl));
+
+///////////////////////// project section animation
